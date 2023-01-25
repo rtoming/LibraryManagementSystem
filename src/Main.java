d6e267f55d5337f5d098f4954ffa6aa5e5895dd6
Rando Toming <rtoming@gmail.com>
2023-01-26 01:31:04 +0200

    Student class - [finished]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-class Student {
+import java.util.HashMap;
+import java.util.Scanner;
 
+class Student {
+    HashMap<Integer, String> studentHasMap = new HashMap<>();
+    Scanner scanner = new Scanner(System.in);
+
+    void addStudent() {
+        studentHasMap.put(101, "Student1");
+        studentHasMap.put(102, "Student2");
+    }
+
+    void regStudent() {
+        System.out.println("Student, enter Your Name: ");
+        String inStudentName = scanner.nextLine();
+        System.out.println("Student, enter Your ID: ");
+        int inStudentID = scanner.nextInt();
+        studentHasMap.put(inStudentID, inStudentName);
+        System.out.println("You are now registered!" + "\n");
+        addStudent();
+        System.out.println("Our members are: " + "\n" + studentHasMap);
+    }
 }
 
 
@@ -20,7 +40,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Student student = new Student();
+        student.regStudent();
 
     }
 }
