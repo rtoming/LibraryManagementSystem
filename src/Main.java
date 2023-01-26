ed70673aabdd06dd834322215f59b2b9b05f35d7
Rando Toming <rtoming@gmail.com>
2023-01-26 02:09:22 +0200

    Issue class - [added and finished issueBook()]

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class Student {
@@ -13,12 +14,12 @@ class Student {
     void regStudent() {
         System.out.println("Student, enter Your Name: ");
         String inStudentName = scanner.nextLine();
-        System.out.println("Student, enter Your ID: ");
+        System.out.println("Student, enter Your ID: " + studentHasMap);
         int inStudentID = scanner.nextInt();
         studentHasMap.put(inStudentID, inStudentName);
         System.out.println("You are now registered!" + "\n");
         addStudent();
-        System.out.println("Our members are: " + "\n" + studentHasMap);
+        System.out.println("Our members now are: " + "\n" + studentHasMap);
     }
 }
 
@@ -33,19 +34,73 @@ class Book {
 }
 
 
-
 class Issue {
+    HashMap<Integer, String> issueHashMap = new HashMap<>();
+    Scanner scanner = new Scanner(System.in);
+
+    Student studentObj = new Student();
+    Book bookObj = new Book();
+
+    Issue() {
+        studentObj.addStudent();
+        bookObj.addBook();
+    }
+
+    void issueBook(int studentID) {
+        if (studentObj.studentHasMap.containsKey(studentID)) {
+            System.out.println("Enter a issued book name: " + bookObj.bookHashMap);
+            String issuedBookName = scanner.nextLine();
+            int bookID = 0;
+
+            if (bookObj.bookHashMap.containsValue(issuedBookName)) {
+                for (Map.Entry<Integer, String> entryIssue : bookObj.bookHashMap.entrySet()) {
+                    if (entryIssue.getValue().equals(issuedBookName)) {
+                        bookID = entryIssue.getKey();
+                        break;
+                    }
+                }
+
+                bookObj.bookHashMap.remove(bookID);
+                    System.out.println("Book is removed from bookHashMap");
+                issueHashMap.put(studentID, issuedBookName);
+                    System.out.println("Book is added to issueHashMap" + "\n");
+
+                String studentName = studentObj.studentHasMap.get(studentID);
+                System.out.println("Name: " + studentName);
+                System.out.println("Book: " + issuedBookName);
+                System.out.println("Book is issued!");
+            } else {
+                System.out.println("We have not such a book in library!");
+                System.out.println("We have following books..." + "\n" + bookObj.bookHashMap);
+            }
+
+        } else {
+            System.out.println("...to issue, before, please reg yourself" + "\n");
+            studentObj.regStudent();
+        }
+    }
+
+    void returnBook() {
+
+    }
+
+    void getPurpose() {
+
+    }
 
 }
-
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Student student = new Student();
-        student.regStudent();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your ID (101, 102, ...): ");
+        int studentID = scanner.nextInt();
+
+        Issue issuedObj = new Issue();
+        issuedObj.issueBook(studentID);
 
     }
 }
